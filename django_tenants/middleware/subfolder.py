3cfb395046bcfd24645594258748e07226871752
Dennis <dennis-on-github@dclabs.de>
2021-01-06 16:15:45 +0100

    Update imports

--- django_tenants/middleware/subfolder.py
+++ django_tenants/middleware/subfolder.py
@@ -1,5 +1,6 @@
 from django.core.exceptions import ImproperlyConfigured
 from django.db import connection
+from django.conf import settings
 from django.http import Http404
 from django.urls import set_urlconf, clear_url_caches
 
